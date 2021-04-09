ff489dc9c4fd96a4f7f1bf2dfdfab9c4ef3142c1
vova <vladimir.gubarenko@gmail.com>
2021-04-09 17:03:48 +0300

    add router 4

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import store from "./store"
+import router from "./router"
 
-const app = createApp(App)
+import Main from "./layouts/Main"
+
+const app = createApp(Main)
 app.use(store)
+app.use(router)
 app.mount('#app')
